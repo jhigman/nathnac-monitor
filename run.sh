0ea6e46ee5a3a5c4e899d8dec6c6b758c1b1c9b1
Julian Higman <jhigman@pobox.com>
2015-07-24 12:47:45 +0000

    Simple ascii output. Invoke scripts in local directory

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sync.sh pages.txt
-send.sh
+./sync.sh pages.txt
+./send.sh
--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 while read line
 do
     parts=($line)
-    echo "curl -s ${parts[0]} | html2text -o ../nathnac-changes/${parts[1]}.txt"
-    curl -s ${parts[0]} | html2text -o ../nathnac-changes/${parts[1]}.txt
+    echo "curl -s ${parts[0]} | html2text -ascii -o ../nathnac-changes/${parts[1]}.txt"
+    curl -s ${parts[0]} | html2text -ascii -o ../nathnac-changes/${parts[1]}.txt
 done < $1
 
