15531cb84b3cf54b7222b6507a90a3f81d3dbaae
Julian Higman <jhigman@pobox.com>
2015-09-02 11:23:51 +0000

    Suppress error warnings from xmllint

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 while read line
 do
     parts=($line)
-    #echo "curl -s -H \"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)\" ${parts[0]} | xmllint --nowarning --html --xpath '//div[@id="tab-container"]' - | html2text -utf8 -style pretty -o ../nathnac-changes/${parts[1]}.txt"
-    curl -s -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)" ${parts[0]} | xmllint --nowarning --html --xpath '//div[@id="tab-container"]' - | html2text -utf8 -style pretty -o ../nathnac-changes/${parts[1]}.txt
+    #echo "curl -s -H \"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)\" ${parts[0]} | xmllint --nowarning --html --xpath '//div[@id="tab-container"]' - 2>/dev/null | html2text -utf8 -style pretty -o ../nathnac-changes/${parts[1]}.txt"
+    curl -s -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)" ${parts[0]} | xmllint --nowarning --html --xpath '//div[@id="tab-container"]' - 2>/dev/null | html2text -utf8 -style pretty -o ../nathnac-changes/${parts[1]}.txt
 done < $1
 
